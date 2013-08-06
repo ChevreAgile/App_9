dd5f4f0a6adceac5a84d0dd8f5fa6b98e239250e
mvcrouse <mvcrouse@indiana.edu>
2013-08-05 21:58:56 -0500

    Added a little intro to eMatch

--- eMatch/eMatch.scm
+++ eMatch/eMatch.scm
@@ -1,3 +1,48 @@
+;Currently this thing REALLY sucks. It's better than nothing, but it doesn't yet support the binding of variables
+;from the pattern expressions to the application expressions
+;
+;
+;Here's some examples of how it works
+
+;Example 1: It finds 5 and returns the right hand side value
+;> (eMatch 5
+;          (7 8)
+;          (3 4)
+;          (5 6))
+;=> 6
+
+;Example 2: It finds (lambda (x y z) z) and returns the right hand side value
+;> (eMatch (lambda (x y z) z)
+;        (7 8)
+;        ((lambda (x y z) z) 4)
+;        (5 6))
+;=> 4
+
+;Example 3: It cannot find a match with (lambda (x y z) z) so it returns an error
+;> (eMatch (lambda (x y z) z)
+;          ((lambda (z) z) 5)
+;          ((lambda (x) x) 6)
+;          ((lambda (y) y) 7))
+;Exception in eMatch: Sooo... You, uh, you messed up something... No match man...
+
+;Example 4: It cannot find a match, but it sees an else statement at the end, so it defaults to that value
+;> (eMatch (lambda (x y z) z)
+;          ((lambda (z) z) 5)
+;          ((lambda (x) x) 6)
+;          ((lambda (y) y) 7)
+;          (else 8))
+;=> 8
+
+;Example 5: '+++' is the symbol for continuing the list, so if the list contains +++ it assumes you don't
+;           care about whatever else is in the list
+;> (eMatch (lambda (x y z) z)
+;          ((lambda (z) z) 5)
+;          ((lambda (x +++) z) 6)
+;          ((lambda (y) y) 7)
+;          (else 8))
+;=> 6
+
+
 (define-syntax eMatch
   (syntax-rules (req else)
     [(_ expr)
